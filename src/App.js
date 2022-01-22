34e659f6d8e3919288c0801b92035827a3d3f542
Javon Hickmon <jj.hickmon@gmail.com>
2022-01-21 17:08:01 -0800

    testing

--- src/App.js
+++ src/App.js
@@ -36,10 +36,10 @@ export default class App extends React.Component {
         fetch('https://api.openweathermap.org/geo/1.0/direct?q='+this.state.city+'&appid=b5b4f0115cb376736a2f740df42c821b')
         .then(data => data.json())
         .then(data => {
-            this.setState({
-                latitude: data[0].lat,
-                longitude: data[0].lon
-            });
+            // this.setState({
+            //     latitude: data[0].lat,
+            //     longitude: data[0].lon
+            // });
             fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+this.state.latitude+'&lon='+this.state.longitude+'&units=imperial&appid=b5b4f0115cb376736a2f740df42c821b')
             .then(response => response.json())
             .then(response => {
