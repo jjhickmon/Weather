12a0b98d6cf26067bed9e6e1c8124ad3fb253211
Javon Hickmon <jj.hickmon@gmail.com>
2022-01-21 17:05:34 -0800

    testing

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ export default class App extends React.Component {
     }
 
     componentDidMount() {
-        this.getLocation();
+        // this.getLocation();
         this.updateWeather();
     }
 
